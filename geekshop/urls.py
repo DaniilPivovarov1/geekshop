f788d10b30fceab342e313a51ae2573b5910ef5f
DaniilPivovarov <daniil.pivovarov2010@gmail.com>
2021-06-07 19:54:36 +0300

    added register, login and logout, and base template

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from products.views import index
 from django.conf.urls.static import static
+
+from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products'))
+    path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
